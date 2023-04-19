d9f59165eb814c077cd330a3e40ac4f22e565ee7
Kulkarni Raunak <kulkarniraunak97@gmail.com>
2023-04-19 13:11:16 +0200

    update test

--- test_newtons_method.py
+++ test_newtons_method.py
@@ -11,6 +11,6 @@ def test_optimisation_class():
     # Initialize the optimization class
     problem_00 = optimisation(x_initial, threshold, num_of_iterations, F)
     # Check if the optimization result is close enough to the expected result
-    expected_result = np.array([[1.75717907], [4.66261117], [3.82115468]])
+    expected_result = np.array([[7.07], [50.0], [-1]])
     result, n_iter = problem_00.newton_method()
     assert np.allclose(result, expected_result, rtol=1e-3, atol=1e-3)
